f9ad33199dc2be1c39946d304b2525098dce2bce
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-07 10:27:50 +0430

    Add last_5_comments and update_post_counter methods to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,19 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
+  has_many :likes
+
+  def update_post_counter
+    author = self.author
+    if author.posts_counter.nil?
+      author.posts_counter = 1
+    else
+      author.posts_counter += 1
+    end
+    author.save
+  end
+
+  def last_5_comments
+    Comment.where(post: self).last(5)
+  end
 end
