08090c6feeb7e96983dcc1330c3d5a8d4e287d12
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-07 10:28:24 +0430

    Add update_comments_counter to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+
+  def update_comments_counter
+    post = self.post
+    post.comments_counter = post.comments_counter.nil? ? 1 : post.comments_counter + 1
+    post.save
+  end
 end
