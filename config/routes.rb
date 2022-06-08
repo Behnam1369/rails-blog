8aebb1c2fe324945b7a9ca556644e574793eb0e9
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-08 13:34:04 +0430

    Route default page to the users page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/users/:user_id/posts', to: 'posts#index'
   get '/users/:user_id/posts/:id', to: 'posts#show'
   get '/users', to: 'users#index'
+  get '/', to: 'users#index'
   get '/users/:id', to: 'users#show'
   # Defines the root path route ("/")
   # root "articles#index"
