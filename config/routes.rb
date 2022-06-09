bfdd21be8568530db117ee85b0a5506193e33bca
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-09 16:00:37 +0430

    Add route configs for new post and editing post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/users/:user_id/posts', to: 'posts#index'
+  get '/posts/:id/edit', to: 'posts#edit'
+  post '/posts/:id/edit', to: 'posts#update'
+  get '/posts/new', to: 'posts#new'
+  post '/posts/new', to: 'posts#create'
   get '/users/:user_id/posts/:id', to: 'posts#show'
   get '/users', to: 'users#index'
   get '/', to: 'users#index'
