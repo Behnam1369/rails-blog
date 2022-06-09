88bb837f3a30838121c66cfb4e6e5ebef35204fa
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-09 16:01:15 +0430

    Add new, create, edit and update actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,37 @@ class PostsController < ApplicationController
   def show
     params
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params['id'])
+  end
+
+  def create
+    @post = Post.create(params.require(:post).permit(:title, :text))
+    @post.author = current_user
+    if @post.save
+      flash[:success] = 'Post saved successfully'
+      redirect_to '/posts/new'
+    else
+      flash.now[:error] = 'Error: Question could not be saved'
+      render :new, locals: { post: }
+    end
+  end
+
+  def update
+    @post = Post.find(params['id'])
+    @post['title'] = params[:post][:title]
+    @post['text'] = params[:post][:text]
+    if @post.save
+      flash[:success] = 'Post saved successfully'
+      redirect_to '/posts/new'
+    else
+      flash.now[:error] = 'Error: Question could not be saved'
+      render :new, locals: { post: }
+    end
+  end
 end
