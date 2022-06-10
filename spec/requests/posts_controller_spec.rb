e2e4248574aa0aab6e3ecf804e238f993c72e5aa
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-10 15:34:14 +0430

    Modify request tests for posts_controller

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,35 +1,47 @@
 require 'rails_helper'
 
-RSpec.describe 'Requesting "/users/5/posts": ', type: :request do
+RSpec.describe 'Requesting "/users/1/posts": ', type: :request do
+  user = User.new(name: 'user1', posts_counter: 0)
+  user.save
+
+  post = Post.new(title: 'New post', text: 'post body', author: user, comments_counter: 0, likes_counter: 0)
+  post.save
+
   it 'should return a response with status code 200.' do
-    get '/users/5/posts'
+    get "/users/#{user.id}/posts"
     expect(response).to have_http_status(200)
   end
 
   it 'should return a response that is a template of "posts/index"' do
-    get '/users/5/posts'
+    get "/users/#{user.id}/posts"
     expect(response).to render_template('posts/index')
   end
 
-  it 'should return a text containing "List of Users:"' do
-    get '/users/5/posts'
-    expect(response.body).to include('List of posts written by users 5')
+  it 'should return a text containing title of the post' do
+    get "/users/#{user.id}/posts"
+    expect(response.body).to include(post.title)
   end
 end
 
-RSpec.describe 'Requesting "/users/5/posts/118": ', type: :request do
+RSpec.describe 'Requesting "/users/1/posts/1": ', type: :request do
+  user = User.new(name: 'user1', posts_counter: 0)
+  user.save
+
+  post = Post.new(title: 'New post', text: 'post body', author: user, comments_counter: 0, likes_counter: 0)
+  post.save
+
   it 'should return a response with status code 200.' do
-    get '/users/5/posts/118'
+    get "/users/#{user.id}/posts/#{post.id}"
     expect(response).to have_http_status(200)
   end
 
   it 'should return a response that is a template of "posts/show"' do
-    get '/users/5/posts/118'
+    get "/users/#{user.id}/posts/#{post.id}"
     expect(response).to render_template('posts/show')
   end
 
-  it 'should return a text containing "List of Users:"' do
-    get '/users/5/posts/118'
-    expect(response.body).to include('Post 118 (author: user5 )')
+  it 'should return a text containing title of post and name of user' do
+    get "/users/#{user.id}/posts/#{post.id}"
+    expect(response.body).to include(post.title) && include(user.name)
   end
 end
