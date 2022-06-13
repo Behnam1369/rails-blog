7d0a28307a20317fe30a47795e66e512537e9db9
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-06-13 17:18:27 -0500

    Add confirmable options, add letter_opener gem, fix error in devlopment.rb
    modified:   Gemfile
    modified:   Gemfile.lock
    modified:   app/models/user.rb
    modified:   config/environments/development.rb
    modified:   db/migrate/20220613174434_add_devise_to_users.rb
    modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_13_175053) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "fname"
     t.string "lname"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
