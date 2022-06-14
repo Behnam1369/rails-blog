c802d0d145d9aa73887aba6c9e59de33de68581c
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-14 16:09:13 +0430

    Add logout button to the layout

--- db/migrate/20220613175053_add_name_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddNameToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :fname, :string
-    add_column :users, :lname, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_13_175053) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_13_174434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,8 +60,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_13_175053) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "fname"
-    t.string "lname"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
