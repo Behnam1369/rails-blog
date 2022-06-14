b0ba2f7fd617ceaf41ae456fb3d77df731fda998
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-15 01:04:41 +0430

    Add delete action to the controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,12 +9,23 @@ class CommentsController < ApplicationController
     post = Post.find(params['post_id'])
     @comment.post = post
     if @comment.save
-      flash[:success] = 'Comment saved successfully'
-      post.comments_counter = post.comments_counter.nil? ? 1 : post.comments_counter + 1
+      flash[:notice] = 'Comment saved successfully'
+      # post.comments_counter = post.comments_counter.nil? ? 1 : post.comments_counter + 1
       post.save
       redirect_to "/users/#{params['user_id']}/posts/#{params['post_id']}"
     else
-      flash.now[:error] = 'Error: Comment could not be saved'
+      flash.now[:alert] = 'Error: Comment could not be saved'
+      render :new, locals: { post: }
+    end
+  end
+
+  def delete
+    @comment = Comment.find(params['id'])
+    if @comment.destroy
+      flash.now[:notice] = 'Comment deleted successfully'
+      # redirect_to "/users/#{params['user_id']}/posts/#{params['post_id']}", notice: 'Comment deleted'
+    else
+      flash.now[:alert] = 'Error: Comment could not be deleted'
       render :new, locals: { post: }
     end
   end
