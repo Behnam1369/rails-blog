a6ce4105a8714fc92cf0d61f8bebc50cdc07c4ec
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-16 21:28:16 +0430

    Update likes_counter after like/unlike

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -61,14 +61,13 @@ class PostsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def like_toggle
     @post = Post.find(params['id'])
-    if Like.where(post: @post, author: current_user).empty?
+    like = Like.where(post: @post, author: current_user)
+    if like.empty?
       like = Like.new(post: @post, author: current_user)
       like.save
-      @post.likes_counter = @post.likes_counter.nil? ? 1 : @post.likes_counter + 1
     else
-      like = Like.where(post: @post, author: current_user)
-      like.destroy_all
-      @post.likes_counter -= 1
+      like = Like.where(post: @post, author: current_user).first
+      like.destroy
     end
     @post.save
     redirect_to "/users/#{params['user_id']}/posts/#{params['id']}"
