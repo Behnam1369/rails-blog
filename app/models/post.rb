3f600f5ff5c24136850655223cfbe38c6cd91c85
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-16 21:24:43 +0430

    Auto update posts_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,13 +7,27 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  after_create :increment_counter_for_post
+  after_destroy :decrement_counter_for_post
+
   def update_post_counter
     author = self.author
-    author.posts_counter = author.posts_counter.nil? ? 1 : author.posts_counter + 1
     author.save
   end
 
   def last_5_comments
     comments.where(post: self).last(5)
+  end
+
+  private
+
+  def increment_counter_for_post
+    author = self.author
+    author.increment!(:posts_counter)
+  end
+
+  def decrement_counter_for_post
+    author = self.author
+    author.decrement!(:posts_counter)
   end
 end
