c5d22b1f34ca00061910f44dd45a6f69b5309331
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-19 13:39:01 +0430

    Config production environment for sending emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,22 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.default_url_options = { host: 'http://rails-blog1000.heruko.app', port: 80 }
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'localhost.com',
+    user_name:            'rails.blog1000@gmail.com',
+    password:             'xuetpfaqbqvipeee',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 } 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
