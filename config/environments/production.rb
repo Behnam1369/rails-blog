cb6568ceb0f5bf85460b707859ea09e4396cae00
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-22 23:59:54 +0430

    Update host address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { host: 'https://rails-blog100.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://rails-blog1000.herokuapp.com/' }
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
